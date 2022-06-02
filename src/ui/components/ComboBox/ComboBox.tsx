fd85e8ff32bbf5e3758b42804ad077a5be327eff
Allan Kildare <allankildaredev@gmail.com>
2022-06-02 17:43:22 -0300

    chore: selected value change in context when select component change

--- src/ui/components/ComboBox/ComboBox.tsx
+++ src/ui/components/ComboBox/ComboBox.tsx
@@ -1,8 +1,12 @@
+import { useContext, useEffect, useState } from 'react'
 import { For } from 'react-extras'
+import { SelectedContestContext } from '../../../contexts/SelectedContestContext'
+import { SelectValues } from '../../../contexts/SelectedContestProvider'
 import { Select } from './styles'
 
 interface Option {
-  text: string
+  id: number
+  nome: string
   value: string
 }
 
@@ -14,17 +18,27 @@ interface ComboBoxProps {
 export function ComboBox({ options, selectedValue = 0 }: ComboBoxProps) {
   const defaultValue = options[selectedValue].value
 
+  const [selectedContest, setSelectedContest] = useState<SelectValues>(
+    defaultValue as SelectValues
+  )
+  const { changeSelectedContest } = useContext(SelectedContestContext)
+
+  function handleContestChange({ target }: any) {
+    setSelectedContest(target.value)
+  }
+
+  useEffect(() => {
+    changeSelectedContest(selectedContest)
+  }, [selectedContest])
+
   return (
-    <Select defaultValue={defaultValue}>
+    <Select defaultValue={defaultValue} onChange={handleContestChange}>
       <For
         of={options}
         render={(item, index) => {
           return (
-            <option
-              key={`option${index}`}
-              value={item.value}
-            >
-              {item.text}
+            <option key={`option${index}`} value={item.value}>
+              {item.nome}
             </option>
           )
         }}
