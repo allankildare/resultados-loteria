2229154115c50931b10f2147ffc21aa2535663e0
Allan Kildare <allankildaredev@gmail.com>
2022-06-02 22:36:40 -0300

    style: adjusting height, width and margin of number box component

--- src/ui/components/NumberBox/styles.ts
+++ src/ui/components/NumberBox/styles.ts
@@ -3,9 +3,10 @@ import styled from 'styled-components'
 export const NumberBoxStyled = styled.div`
     background-color: #FFF;
     border-radius: 50%;
-    width: 64px;
-    height: 64px;
+    min-width: 64px;
+    min-height: 64px;
     font-weight: 700;
+    margin: 0 1rem 1rem 0;
 
     display: flex;
     align-items: center;
