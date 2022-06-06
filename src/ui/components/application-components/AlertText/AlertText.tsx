fcf2db510d0a4973467ae81ff465125d75ea3331
Allan Kildare <allankildaredev@gmail.com>
2022-06-06 15:49:12 -0300

    feat: adding alert text component

---
+++ src/ui/components/application-components/AlertText/AlertText.tsx
@@ -0,0 +1,20 @@
+import { StyledAlertText } from './styles'
+import { Color } from '~/types'
+
+const DEFAULT_MESSAGE = 'Infelizmente ocorreu um erro durante a busca, pesquise outro concurso ou tente novamente mais tarde.'
+
+interface AlertTextProps {
+    as?: 'p' | 'span'
+    message?: string
+    color?: Color
+}
+
+export function AlertText(props: AlertTextProps) {
+    const { message = DEFAULT_MESSAGE, as, color } = props
+
+    return (
+        <StyledAlertText as={as} color={color}>
+            {message}
+        </StyledAlertText>
+    )
+}
---
+++ src/ui/components/application-components/AlertText/styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const StyledAlertText = styled.p`
+    color: ${({color}) => color || '#e84338'};
+    font-weight: 500;
+`
