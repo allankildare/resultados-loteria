2e02f0591105a24823ec409dad1dec9b3c160ecf
Allan Kildare <allankildaredev@gmail.com>
2022-06-06 13:38:55 -0300

    feat: configuring tilde path as absolute path

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from 'path'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
@@ -7,8 +8,12 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
     define: {
-      'process.env': env
+      'process.env': env,
+    },
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, './src')
+      }
     }
   }
-}
-)
+})
