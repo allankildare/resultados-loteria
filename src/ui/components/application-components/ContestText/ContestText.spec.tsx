b4be87b15aeaa443baaa18bc3ceb8f53200e571f
allankildare <--global>
2022-06-22 01:00:53 -0300

    test: improving tests of contest text component

--- src/ui/components/application-components/ContestText/ContestText.spec.tsx
+++ src/ui/components/application-components/ContestText/ContestText.spec.tsx
@@ -3,11 +3,14 @@ import { render, screen } from '@testing-library/react'
 import { ContestText } from './ContestText'
 
 describe('<ContestText /> component', () => {
-    it('should render component with static text', () => {
+    it('should render component with text props', () => {
         render(<ContestText contestId="2200" contestDate="2002-01-27T06:10:00.000Z" />)
         
         expect(screen.getByTestId('contestText')).toBeInTheDocument()
         expect(screen.getByText(/2200/i)).toBeInTheDocument()
         expect(screen.getByText('2200 - 27/01/2002')).toBeInTheDocument()
+        expect(screen.getByText('2200 - 27/01/2002')).toHaveStyle({ fontWeight: '700' })
+        expect(screen.getByText(/Concurso/i)).toBeInTheDocument()
+        expect(screen.getByText(/Concurso/i)).toHaveStyle({ fontWeight: '500' })
     })
 })
