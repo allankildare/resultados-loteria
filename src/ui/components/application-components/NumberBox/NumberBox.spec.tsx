058bcafda9ca514b5d3fdb346bcffb4198556fa7
Allan Kildare <allankildaredev@gmail.com>
2022-06-28 16:22:16 -0300

    test: separating tests for numbers less than 10 and greater than -1

--- src/ui/components/application-components/NumberBox/NumberBox.spec.tsx
+++ src/ui/components/application-components/NumberBox/NumberBox.spec.tsx
@@ -3,8 +3,25 @@ import React from 'react'
 import { NumberBox } from './NumberBox'
 
 describe('<NumberBox /> component', () => {
-    it('should render numbers less than 10 and greater than -1 with the leading digit zero', () => {
-        render(<NumberBox number={4} />)
-        expect(screen.getByTestId('numberBox')).toHaveTextContent('04')
+  describe('rendering numbers less than 10 and greater than -1 with the leading digit zero', () => {
+    it('should render "01" as text content', () => {
+      render(<NumberBox number={1} />)
+      expect(screen.getByTestId('numberBox')).toHaveTextContent('01')
     })
+
+    it('should render "04" as text content', () => {
+      render(<NumberBox number={4} />)
+      expect(screen.getByTestId('numberBox')).toHaveTextContent('04')
+    })
+
+    it('should render "07" as text content', () => {
+      render(<NumberBox number={7} />)
+      expect(screen.getByTestId('numberBox')).toHaveTextContent('07')
+    })
+
+    it('should render "09" as text content', () => {
+      render(<NumberBox number={9} />)
+      expect(screen.getByTestId('numberBox')).toHaveTextContent('09')
+    })
+  })
 })
