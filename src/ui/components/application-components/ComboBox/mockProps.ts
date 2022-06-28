ff53702834ac085e05af724d2a066d736a94bd41
Allan Kildare <allankildaredev@gmail.com>
2022-06-28 16:25:44 -0300

    feat: adding mockProps file to comboBox component

---
+++ src/ui/components/application-components/ComboBox/mockProps.ts
@@ -0,0 +1,8 @@
+import type { Option } from './interface'
+
+export const mockOptions: Option[] = [
+    { id: 0, nome: 'Opção 1', value: 'opcao_1' },
+    { id: 1, nome: 'Opção 2', value: 'opcao_2' },
+    { id: 2, nome: 'Opção 3', value: 'opcao_3' },
+    { id: 3, nome: 'Opção 4', value: 'opcao_4' }
+]
