15a19597baf27ef9da0fb04e081fe3b9c5dff833
Allan Kildare <allankildaredev@gmail.com>
2022-07-07 20:20:36 -0300

    chore: using vite isolated environment

--- src/services/endpoints.ts
+++ src/services/endpoints.ts
@@ -1,4 +1,4 @@
-export const API_ENDPOINT = process.env.API_ENDPOINT
+export const API_ENDPOINT = import.meta.env.VITE_API_ENDPOINT
 export const LOTTERIES_ENDPOINT = '/loterias'
 export const IDS_ENDPOINT = '/loterias-concursos'
 export const CONTEST_ENDPOINT = '/concursos'
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,12 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import * as path from 'path'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-
-  return {
-    plugins: [react()],
-    define: {
-      'process.env': env,
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './src'),
     },
-    resolve: {
-      alias: {
-        '~': path.resolve(__dirname, './src')
-      }
-    }
-  }
+  },
 })
